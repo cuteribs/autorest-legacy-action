c899a5921afac5d7a2903519ed7af7283873cb88
cuteribs <85863433+cuteribs@users.noreply.github.com>
2023-11-20 04:02:50 +0800

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ csharp=${5-false}
 typescript=${6-false}
 python=${7-false}
 
-mv *.md /github/home/.autorest/@autorest_core@3.5.1/node_modules/@autorest/core/dist/resources/
+ls -lR
 
-export PATH=$PATH:/github/home/.autorest/.dotnet
+mv *.md ~/.autorest/@autorest_core@3.5.1/node_modules/@autorest/core/dist/resources/
+
+export PATH=$PATH:~/.dotnet
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 autorest \
@@ -26,6 +28,6 @@ autorest \
   --python:$python \
   --use:@cuteribs/autorest.csharp@latest \
   --use:@cuteribs/autorest.python@latest \
-  --use:@cuteribs/autorest.typescript@latest \
-  --verbose
+  --use:@cuteribs/autorest.typescript@latest
+  
 
